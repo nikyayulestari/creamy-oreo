df4844e5a1bf744a6b73cb11f00f0b873a98353a
Niky Ayu Lestari <nikyayulestari@gmail.com>
2019-10-13 18:57:32 +0700

    Source Code Hitung Nilai Tugas Siswa

---
+++ NilaiSiswa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+float Nilai(float benar, float
+total)
+{
+float Sementara;
+Sementara=(100*benar/total);
+return Sementara;
+}
+
+int main()
+{
+float A, B, C;
+float Nilai(float benar,
+float total);
+cin>>A;
+cin>>B;
+C = Nilai(A,B);
+cout<<C;
+return 0;
+}
