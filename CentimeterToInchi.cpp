7128957b4b0bd25049f686646424f2bc5ea3dac1
Niky Ayu Lestari <nikyayulestari@gmail.com>
2019-10-13 18:56:26 +0700

    Mengubah Nilai Centimeter ke Inchi

---
+++ CentimeterToInchi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+float Inchi(float cm)
+{
+float Sementara;
+Sementara=cm/2.54;
+return Sementara;
+}
+
+int main()
+{
+float A, C;
+float Inchi(float cm);
+cin>>A;
+C = Inchi(A);
+cout<<C;
+return 0;
+}
